0ca89743ab7ed12eb81e2f9f63711bc202cd5fbe
salimtahayacine <salimtahayacine@gmail.com>
2025-04-09 13:43:26 +0100

    Adding some style

--- ToDo-app/frontend/src/components/Navbar.js
+++ ToDo-app/frontend/src/components/Navbar.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">TodoApp</Link>
-      </div>
-      <ul className="navbar-links">
-        <li><Link to="/login">Se connecter</Link></li>
-        <li><Link to="/register">S'inscrire</Link></li>
-      </ul>
-    </nav>
+    <>
+      {/* Bootstrap CSS from CDN */}
+      <link 
+        rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
+        crossOrigin="anonymous"
+      />
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container">
+          <Link className="navbar-brand fw-bold" to="/">
+            TodoApp
+          </Link>
+          <button 
+            className="navbar-toggler" 
+            type="button"
+            onClick={toggleNavbar}
+            aria-controls="navbarNav" 
+            aria-expanded={isOpen ? "true" : "false"} 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Se connecter
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  S'inscrire
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
